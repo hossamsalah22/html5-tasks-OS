0554bcd99b88a27ea9e0eb53d312a22d386425bf
Hossam Salah <hossamsalahabbas@gmail.com>
2023-01-16 10:05:58 +0200

    added some features

--- Day01LabSolution/Task02/js/main.js
+++ Day01LabSolution/Task02/js/main.js
@@ -8,4 +8,12 @@ window.addEventListener("load", function () {
 	document.querySelectorAll("button")[1].addEventListener("click", function () {
 		box.classList.remove("start");
 	});
+
+	box.addEventListener("mouseover", function () {
+		box.classList.add("pause");
+	});
+
+	box.addEventListener("mouseout", function () {
+		box.classList.remove("pause");
+	});
 });
